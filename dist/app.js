e5878ec67e84453483eb24533e2c8b307233e507
[vinald] <okiror1vinald@gmail.com>
2025-07-29 21:16:26 +0300

    displaying the created expense item on UI

--- dist/app.js
+++ dist/app.js
@@ -3,21 +3,53 @@ const expenseType = document.getElementById("expense-type");
 const expenseDescription = document.getElementById("desc");
 const expenseAmount = document.getElementById("amount");
 const addExpenseButton = document.querySelector(".add-expense-btn");
+const creditDiv = document.querySelector(".expense-credit-item-container");
+const debitDiv = document.querySelector(".expense-debit-item-container");
+let expenseItems = [];
 class Expense {
     constructor(type, description, amount) {
         this.id = 0;
-        this.type = "credit";
+        this.type = "debit";
         this.description = "";
         this.amount = 0;
         this.type = type;
         this.description = description;
         this.amount = amount;
-        this.id = Expense.curentId++;
+        this.id = Expense.currentId++;
+    }
+    getId() {
+        return this.id;
+    }
+    getDescription() {
+        return this.description;
+    }
+    getAmount() {
+        return this.amount;
     }
 }
-Expense.curentId = 0;
+Expense.currentId = 0;
+const displayExpenseItems = () => {
+    debitDiv.innerHTML = "";
+    creditDiv.innerHTML = "";
+    for (let item = 0; item < expenseItems.length; item++) {
+        let expenseItem = expenseItems[item];
+        let containerDiv = expenseItem.type === "credit" ? creditDiv : debitDiv;
+        let cssClass = expenseItem.type === "credit" ? "credit-item" : "debit-item";
+        let template = `
+            <div class="${cssClass}">
+                <div class="exp-desc">${expenseItem.getDescription()}</div>
+                <div class="exp-amount">${expenseItem.getAmount()}</div>
+                <div class="expense-delete">
+                    <button class="delete-expense-button">X</button>
+                </div>
+            </div>`;
+        containerDiv === null || containerDiv === void 0 ? void 0 : containerDiv.insertAdjacentHTML("beforeend", template);
+    }
+};
 addExpenseButton.addEventListener("click", (event) => {
     event.preventDefault();
-    const expense = new Expense("credit", "test", 100);
-    console.log(expense);
+    let type = expenseType.value === "credit" ? "credit" : "debit";
+    const expense = new Expense(type, expenseDescription.value, expenseAmount.valueAsNumber);
+    expenseItems.push(expense);
+    displayExpenseItems();
 });
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
-const expenseType = document.getElementById("expense-type")! as HTMLInputElement;
+const expenseType = document.getElementById("expense-type")! as HTMLSelectElement;
 const expenseDescription = document.getElementById("desc")! as HTMLInputElement;
 const expenseAmount = document.getElementById("amount")! as HTMLInputElement;
 const addExpenseButton = document.querySelector(".add-expense-btn")! as HTMLButtonElement;
+const creditDiv = document.querySelector(".expense-credit-item-container")! as HTMLDivElement;
+const debitDiv = document.querySelector(".expense-debit-item-container")! as HTMLDivElement;
+
+let expenseItems: Expense[] = [];
 
 class Expense {
-    static curentId: number = 0;
-    id: number = 0;
-    type: "credit" | "debit" = "credit";
+    private static currentId: number = 0;
+    private readonly id: number = 0;
+    type: "credit" | "debit" = "debit";
     description: string = "";
     amount: number = 0;
 
@@ -14,12 +18,46 @@ class Expense {
         this.type = type;
         this.description = description;
         this.amount = amount;
-        this.id = Expense.curentId++;
+        this.id = Expense.currentId++;
+    }
+
+    getId(): number {
+        return this.id;
+    }
+    getDescription(): string {
+        return this.description;
+    }
+    getAmount(): number {
+        return this.amount;
     }
 }
 
+const displayExpenseItems = () => {
+    debitDiv.innerHTML = "";
+    creditDiv.innerHTML = "";
+    for (let item = 0; item < expenseItems.length; item++) {
+        let expenseItem = expenseItems[item];
+        let containerDiv = expenseItem.type === "credit" ? creditDiv : debitDiv;
+
+        let cssClass = expenseItem.type === "credit" ? "credit-item" : "debit-item";
+        let template = `
+            <div class="${cssClass}">
+                <div class="exp-desc">${expenseItem.getDescription()}</div>
+                <div class="exp-amount">${expenseItem.getAmount()}</div>
+                <div class="expense-delete">
+                    <button class="delete-expense-button">X</button>
+                </div>
+            </div>`;
+        containerDiv?.insertAdjacentHTML("beforeend", template);
+    }
+};
+
 addExpenseButton.addEventListener("click", (event) => {
     event.preventDefault();
-    const expense = new Expense("credit", "test", 100);
-    console.log(expense);
+
+    let type: "credit" | "debit" = expenseType.value === "credit" ? "credit" : "debit";
+    const expense = new Expense(type, expenseDescription.value, expenseAmount.valueAsNumber);
+    expenseItems.push(expense);
+
+    displayExpenseItems();
 });
